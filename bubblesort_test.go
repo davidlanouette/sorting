2cef181b46da01169981964de49efb1bbba291dc
David Lanouette <David.Lanouette@Bronto.com>
2018-10-03 20:02:22 -0400

    Change the way the assert lib is used.
    Now we are using the raw methods, and not instanciating a new assert object.

--- bubblesort_test.go
+++ bubblesort_test.go
@@ -11,19 +11,15 @@ func TestBubbleSortNil(t *testing.T) {
 }
 
 func TestBubbleSortShortList(t *testing.T) {
-	assert := assert.New(t)
-
 	input := []int{5, 4, 3, 2, 1}
 	output := BubbleSort(input)
 
-	assert.Equal([]int{1, 2, 3, 4, 5}, output)
+	assert.Equal(t, []int{1, 2, 3, 4, 5}, output)
 }
 
 func TestBubbleSortRandomList(t *testing.T) {
-	assert := assert.New(t)
-
 	input := []int{2325, 2432, 8873, 34966, 4020, 5456, 42285}
 	output := BubbleSort(input)
 
-	assert.Equal([]int{2325, 2432, 4020, 5456, 8873, 34966, 42285}, output)
+	assert.Equal(t, []int{2325, 2432, 4020, 5456, 8873, 34966, 42285}, output)
 }
